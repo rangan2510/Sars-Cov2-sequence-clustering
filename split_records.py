645f590891a44f22b15e0307a5ed2009c923297a
Rangan Das <970480+rangan2510@users.noreply.github.com>
2020-06-02 10:35:58 +0530

    bug fixes

--- split_records.py
+++ split_records.py
@@ -1,6 +1,23 @@
 #%%
 from Bio import SeqIO
 
-seq_record = SeqIO.parse("Sars-Cov2-nucleotides.fasta", "fasta")
+nucleotides = list(SeqIO.parse("Sars-Cov2-nucleotides.fasta", "fasta"))
+proteins = list(SeqIO.parse("Sars-Cov2-proteins.fasta", "fasta"))
+
+#%%
+path = "nucleotides/"
+
+#%%
+print("Writing nucleotides...\n")
+from tqdm import tqdm
+for record in tqdm(nucleotides):
+    handle = path + str(record.id) + ".fsa"
+    SeqIO.write(record, handle,"fasta")
 
 # %%
+path = "proteins/"
+print("Writing proteins...\n")
+from tqdm import tqdm
+for record in tqdm(proteins):
+    handle = path + str(record.id) + ".fsa"
+    SeqIO.write(record, handle,"fasta")
